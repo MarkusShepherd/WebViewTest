2fc7df9d780af1fa9b25fc3e70785f9bc70d2128
Markus Schepke <markus.schepke@censhare.com>
2014-01-22 18:19:23 +0000

    added audio file and media player

--- src/info/riemannhypothesis/android/webviewtest/JabberwockyActivity.java
+++ src/info/riemannhypothesis/android/webviewtest/JabberwockyActivity.java
@@ -1,12 +1,14 @@
 package info.riemannhypothesis.android.webviewtest;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.webkit.WebView;
 
 public class JabberwockyActivity extends Activity {
 
 	WebView webView;
+	MediaPlayer myPlayer;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +16,21 @@ public class JabberwockyActivity extends Activity {
 		setContentView(R.layout.activity_web);
 		webView = (WebView) findViewById(R.id.webView);
 		webView.loadUrl("file:///android_asset/jabberwocky.html");
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		myPlayer = MediaPlayer.create(this, R.raw.schepke_opus_1);
+		myPlayer.setLooping(true);
+		myPlayer.start();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		myPlayer.stop();
+		myPlayer.release();
 	}
 
 	@Override
